6defbc3f22f1966866440be5574557eb6a4ca45c
Hao Yu <yuhayo@mail.ru>
2022-09-24 20:06:40 +0300

    ykaz

--- Source.cpp
+++ Source.cpp
@@ -1,40 +1,61 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
-int main() {
-	const int n = 10;
-	int m = 0;
-	int c,num;
-	int a[n];
-	for (int i = 0; i < n; i++)
+int main()
+{
+	const int N = 10;
+	int r = 0, l = 0, loc_len = 0, max_len = 0, index = 0, c = 0, z = 0, flag = 0;
+	int a[N];
+	srand(time(0));
+	for (int i = 0; i < N; i++)
 	{
-		cin >>a[i];
+		a[i] = rand() % 2;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		printf_s("%d", a[i]);
 	}
 
-	for (int i = 0; i < n; i++)
+	while (r < N)
 	{
-		if (a[i] == 0) 
+		loc_len = 0;
+		flag = 0;
+		while (a[r] == 1)
 		{
-			a[i] = 1;
-			c = 0;
-			for (int j = 0; j < n; j++)
+			r++;
+			loc_len++;
+		}
+		loc_len++;
+		r++;
+		while (a[r] == 1)
+		{
+			if ((flag == 0) and (a[r - 1] == 0))
 			{
-				if (a[j] == 1)
-				{
-					c += 1;
-					if (c > m)
-					{
-						m = c;
-						num = i;
-					}
-				}
-				else 
-				{
-					c = 0;
-				}
+				l = r;
+				flag++;
 			}
-			a[i] = 0;
+			r++;
+			loc_len++;
+		}
+		if (loc_len > max_len)
+		{
+			max_len = loc_len;
+			index = l;
+		}
+		if (flag == 0)
+		{
+			l++;
+		}
+		if (r < N)
+		{
+			r = l;
+		}
+		else
+		{
+			break;
 		}
 	}
-	cout <<" max lenght "<< m <<" index "<< num+1;
+	cout << "   " << index;
+	return 0;
 }
